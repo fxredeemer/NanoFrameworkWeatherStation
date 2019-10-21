11241ac073cb10d46310263826024e20c810dbe4
Fx137 <flufeu@gmail.com>
2019-10-21 20:58:08 +0200

    initial commit

---
+++ WheaterStation/Periphery/TemperatureSensor.cs
@@ -0,0 +1,29 @@
+﻿using Windows.Devices.I2c;
+
+namespace WheaterStation.Periphery
+{
+    class TemperatureSensor
+    {
+        readonly I2cDevice i2CDevice;
+
+        public TemperatureSensor(I2cDevice i2CDevice)
+        {
+            this.i2CDevice = i2CDevice;
+        }
+
+        public static TemperatureSensor Create()
+        {
+            var connectionSettings = new I2cConnectionSettings(17);
+            var i2CDevice = I2cDevice.FromId("", connectionSettings);
+            return new TemperatureSensor(i2CDevice);
+        }
+
+        public double ReadTemperature()
+        {
+            var buffer = new byte[33];
+            i2CDevice.Read(buffer);
+
+            return 1.3;
+        }
+    }
+}
---
+++ WheaterStation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using nanoFramework.Hardware.Esp32;
+
+namespace WheaterStation
+{
+    public class Program
+    {
+        public static ManualResetEvent _touchEvent;
+
+        public static void Main()
+        {
+            var i2cData = Configuration.GetFunctionPin(DeviceFunction.I2C1_DATA);
+            var i2cClock = Configuration.GetFunctionPin(DeviceFunction.I2C1_CLOCK);
+
+            Configuration.SetPinFunction(i2cData, DeviceFunction.I2C1_DATA);
+            Configuration.SetPinFunction(i2cClock, DeviceFunction.I2C1_CLOCK);
+
+            Sleep.EnableWakeupByTimer(TimeSpan.FromSeconds(5));
+
+            using (var timer = new Timer(TimerTick, null, 5000, 5000))
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+
+        private static void TimerTick(object state)
+        {
+
+        }
+    }
+}
