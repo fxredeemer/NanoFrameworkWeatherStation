8ef7a99715d59eca37ab0f71b5b1221f2db264c7
Fx137 <flufeu@gmail.com>
2019-10-22 18:23:12 +0200

    added display scaffold class

---
+++ WheaterStation/Periphery/Display.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Windows.Devices.I2c;
+
+namespace WheaterStation.Periphery
+{
+    internal class Display
+    {
+        private readonly I2cDevice i2CDevice;
+
+        public Display(I2cDevice i2CDevice)
+        {
+            this.i2CDevice = i2CDevice;
+        }
+
+        public static Display CreateNew()
+        {
+            var connectionSettings = new I2cConnectionSettings(0x17);
+            var i2CDevice = I2cDevice.FromId("", connectionSettings);
+            return new Display(i2CDevice);
+        }
+
+        public void WriteLine(int line, string value)
+        {
+            var a = Encoding.UTF8.GetBytes(value);
+
+            i2CDevice.Write(a);
+
+        }
+    }
+}
--- WheaterStation/Periphery/TemperatureSensor.cs
+++ WheaterStation/Periphery/TemperatureSensor.cs
@@ -4,16 +4,16 @@ namespace WheaterStation.Periphery
 {
     class TemperatureSensor
     {
-        readonly I2cDevice i2CDevice;
+        readonly I2cDevice i2cDevice;
 
-        public TemperatureSensor(I2cDevice i2CDevice)
+        public TemperatureSensor(I2cDevice i2cDevice)
         {
-            this.i2CDevice = i2CDevice;
+            this.i2cDevice = i2cDevice;
         }
 
-        public static TemperatureSensor Create()
+        public static TemperatureSensor CreateNew()
         {
-            var connectionSettings = new I2cConnectionSettings(17);
+            var connectionSettings = new I2cConnectionSettings(0xA1);
             var i2CDevice = I2cDevice.FromId("", connectionSettings);
             return new TemperatureSensor(i2CDevice);
         }
@@ -21,9 +21,17 @@ namespace WheaterStation.Periphery
         public double ReadTemperature()
         {
             var buffer = new byte[33];
-            i2CDevice.Read(buffer);
+            i2cDevice.Read(buffer);
 
             return 1.3;
+        }
+
+        public double ReadHumidity()
+        {
+            var buffer = new byte[33];
+            i2cDevice.Read(buffer);
+
+            return 2.5;
         }
     }
 }
