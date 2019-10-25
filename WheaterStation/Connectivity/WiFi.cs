e73d932fd7221b3b0a05ce7e2d25e940e0cb5b9f
Flurin Feuerstein <flufeu@gmail.com>
2019-10-25 20:03:57 +0200

    some minor chnages

--- WheaterStation/Connectivity/WiFi.cs
+++ WheaterStation/Connectivity/WiFi.cs
@@ -1,41 +1,50 @@
-﻿//using System;
-//using System.Text;
-//using Windows.Devices.WiFi;
+﻿using System;
+using System.Threading;
+using Windows.Devices.WiFi;
 
-//namespace WheaterStation.Connectivity
-//{
-//    internal class WiFi
-//    {
-//        private readonly WiFiAdapter wiFiAdapter;
+namespace WheaterStation.Connectivity
+{
+    internal class WiFi
+    {
+        private readonly WiFiAdapter wiFiAdapter;
+        Timer timer;
 
-//        public WiFi()
-//        {
-//            wiFiAdapter = WiFiAdapter.FindAllAdapters()[0];
-//        }
+        public WiFi()
+        {
+            wiFiAdapter = WiFiAdapter.FindAllAdapters()[0];
+            wiFiAdapter.AvailableNetworksChanged += AvailableNetworksChanged;
 
-//        public void StartSniffing()
-//        {
-//            wiFiAdapter.AvailableNetworksChanged += AvailableNetworksChanged;
-//        }
+        }
 
-//        public void StopSniffing()
-//        {
-//            wiFiAdapter.AvailableNetworksChanged -= AvailableNetworksChanged;
-//        }
+        public void StartSniffing()
+        {
+            timer = new Timer(ScanWifi, null, 0, 15000);
+        }
 
-//        private void AvailableNetworksChanged(WiFiAdapter sender, object e)
-//        {
-//            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
+        public void StopSniffing()
+        {
+            timer.Dispose();
+        }
 
-//            // Get Report of all scanned WiFi networks
-//            WiFiNetworkReport report = sender.NetworkReport;
+        private void ScanWifi(object state)
+        {
+            Console.WriteLine("scanning wifi");
+            wiFiAdapter.ScanAsync();
+        }
 
-//            // Enumerate though networks looking for our network
-//            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
-//            {
-//                // Show all networks found
-//                Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
-//            }
-//        }
-//    }
-//}
+        private void AvailableNetworksChanged(WiFiAdapter sender, object e)
+        {
+            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
+
+            // Get Report of all scanned WiFi networks
+            var report = sender.NetworkReport;
+
+            // Enumerate though networks looking for our network
+            foreach (var net in report.AvailableNetworks)
+            {
+                // Show all networks found
+                Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
+            }
+        }
+    }
+}
--- WheaterStation/Program.cs
+++ WheaterStation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using nanoFramework.Hardware.Esp32;
 using System;
 using System.Threading;
+using WheaterStation.Connectivity;
 using Windows.Devices.Gpio;
 using Windows.Devices.WiFi;
 
@@ -19,20 +20,10 @@ namespace WheaterStation
 
             try
             {
-                // Get the first WiFI Adapter
-                WiFiAdapter wifi = WiFiAdapter.FindAllAdapters()[0];
+                var wifi = new WiFi();
+                wifi.StartSniffing();
 
-                // Set up the AvailableNetworksChanged event to pick up when scan has completed
-                wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
-
-                // Loop forever scanning every 30 seconds
-                while (true)
-                {
-                    Console.WriteLine("starting WiFi scan");
-                    wifi.ScanAsync();
-
-                    Thread.Sleep(30000);
-                }
+                Thread.Sleep(Timeout.Infinite);
             }
             catch (Exception ex)
             {
@@ -40,29 +31,6 @@ namespace WheaterStation
                 Console.WriteLine("stack:" + ex.StackTrace);
             }
 
-
-            while (true)
-            {
-                pin.Write(GpioPinValue.High);
-                Thread.Sleep(1000);
-                pin.Write(GpioPinValue.Low);
-                Thread.Sleep(1000);
-            }
-        }
-
-        private static void Wifi_AvailableNetworksChanged(WiFiAdapter sender, object e)
-        {
-            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
-
-            // Get Report of all scanned WiFi networks
-            WiFiNetworkReport report = sender.NetworkReport;
-
-            // Enumerate though networks looking for our network
-            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
-            {
-                // Show all networks found
-                Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
-            }
         }
     }
 }
