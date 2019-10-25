fb0a8690eb05d76c1efedd810b456661b6c75d0d
Flurin Feuerstein <flufeu@gmail.com>
2019-10-25 19:53:20 +0200

    managed mo make it work :-D

---
+++ WheaterStation/Connectivity/WiFi.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Text;
+//using Windows.Devices.WiFi;
+
+//namespace WheaterStation.Connectivity
+//{
+//    internal class WiFi
+//    {
+//        private readonly WiFiAdapter wiFiAdapter;
+
+//        public WiFi()
+//        {
+//            wiFiAdapter = WiFiAdapter.FindAllAdapters()[0];
+//        }
+
+//        public void StartSniffing()
+//        {
+//            wiFiAdapter.AvailableNetworksChanged += AvailableNetworksChanged;
+//        }
+
+//        public void StopSniffing()
+//        {
+//            wiFiAdapter.AvailableNetworksChanged -= AvailableNetworksChanged;
+//        }
+
+//        private void AvailableNetworksChanged(WiFiAdapter sender, object e)
+//        {
+//            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
+
+//            // Get Report of all scanned WiFi networks
+//            WiFiNetworkReport report = sender.NetworkReport;
+
+//            // Enumerate though networks looking for our network
+//            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
+//            {
+//                // Show all networks found
+//                Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
+//            }
+//        }
+//    }
+//}
--- WheaterStation/Program.cs
+++ WheaterStation/Program.cs
@@ -1,32 +1,68 @@
-﻿using System;
+﻿using nanoFramework.Hardware.Esp32;
+using System;
 using System.Threading;
-using nanoFramework.Hardware.Esp32;
+using Windows.Devices.Gpio;
+using Windows.Devices.WiFi;
 
 namespace WheaterStation
 {
-    public class Program
+    static class Programm
     {
-        public static ManualResetEvent _touchEvent;
-
         public static void Main()
         {
-            var i2cData = Configuration.GetFunctionPin(DeviceFunction.I2C1_DATA);
-            var i2cClock = Configuration.GetFunctionPin(DeviceFunction.I2C1_CLOCK);
+            Console.WriteLine("Starting!");
 
-            Configuration.SetPinFunction(i2cData, DeviceFunction.I2C1_DATA);
-            Configuration.SetPinFunction(i2cClock, DeviceFunction.I2C1_CLOCK);
+            var gpioController = new GpioController();
+            var pin = gpioController.OpenPin(Gpio.IO13);
 
-            Sleep.EnableWakeupByTimer(TimeSpan.FromSeconds(5));
+            pin.SetDriveMode(GpioPinDriveMode.Output);
 
-            using (var timer = new Timer(TimerTick, null, 5000, 5000))
+            try
             {
-                Thread.Sleep(Timeout.Infinite);
+                // Get the first WiFI Adapter
+                WiFiAdapter wifi = WiFiAdapter.FindAllAdapters()[0];
+
+                // Set up the AvailableNetworksChanged event to pick up when scan has completed
+                wifi.AvailableNetworksChanged += Wifi_AvailableNetworksChanged;
+
+                // Loop forever scanning every 30 seconds
+                while (true)
+                {
+                    Console.WriteLine("starting WiFi scan");
+                    wifi.ScanAsync();
+
+                    Thread.Sleep(30000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("message:" + ex.Message);
+                Console.WriteLine("stack:" + ex.StackTrace);
+            }
+
+
+            while (true)
+            {
+                pin.Write(GpioPinValue.High);
+                Thread.Sleep(1000);
+                pin.Write(GpioPinValue.Low);
+                Thread.Sleep(1000);
             }
         }
 
-        private static void TimerTick(object state)
+        private static void Wifi_AvailableNetworksChanged(WiFiAdapter sender, object e)
         {
+            Console.WriteLine("Wifi_AvailableNetworksChanged - get report");
 
+            // Get Report of all scanned WiFi networks
+            WiFiNetworkReport report = sender.NetworkReport;
+
+            // Enumerate though networks looking for our network
+            foreach (WiFiAvailableNetwork net in report.AvailableNetworks)
+            {
+                // Show all networks found
+                Console.WriteLine($"Net SSID :{net.Ssid},  BSSID : {net.Bsid},  rssi : {net.NetworkRssiInDecibelMilliwatts.ToString()},  signal : {net.SignalBars.ToString()}");
+            }
         }
     }
 }
